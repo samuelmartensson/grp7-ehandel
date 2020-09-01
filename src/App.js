2692ffd19cf8c98c3d71c3ec971b1e0aabb60258
Samuel Mårtensson <samuel137@live.se>
2020-09-01 14:19:20 +0200

    fixing for merge

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import ProductList from './components/ProductList';
-import { DataContext } from './contexts/DataContext';
-import ProductDetailPage from './components/ProductDetailPage';
+import React from 'react';
 
 function App() {
-  const [data, setData] = useState({});
-  useEffect(() => {
-    fetch('https://mock-data-api.firebaseio.com/e-commerce/.json')
-      .then((res) => res.json())
-      .then((items) => {
-        setData(items);
-      });
-  }, []);
-
-  return (
-    <div className="App">
-      <DataContext.Provider value={data}>
-        <ProductList />
-        <ProductDetailPage />
-      </DataContext.Provider>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
