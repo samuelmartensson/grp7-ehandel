774d52b984eec52e71b21df0d5b26f9f0e5d29dd
Samuel Mårtensson <samuel137@live.se>
2020-09-01 17:33:28 +0200

    sorted cart items by price

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -52,26 +52,28 @@ export default function Cart() {
     <div className="cart">
       <h2 className="cart__subheader">Your cart</h2>
       <div className="cart__item-container">
-        {products.map((product, i) => {
-          return (
-            <div className="cart__item" key={i}>
-              <div className="cart__img-wrap">
-                <img src={product.images[0].src.small} alt="" />
+        {products
+          .sort((a, b) => b.price - a.price)
+          .map((product, i) => {
+            return (
+              <div className="cart__item" key={i}>
+                <div className="cart__img-wrap">
+                  <img src={product.images[0].src.small} alt="" />
+                </div>
+                <div className="cart__info-wrap">
+                  <div className="cart__name">{product.name}</div>
+                  <div className="cart__description">{product.description}</div>
+                </div>
+                <div className="cart__price">{product.price} SEK</div>
+                <button
+                  onClick={() => handleRemove(product.id)}
+                  className="cart__remove"
+                >
+                  Remove
+                </button>
               </div>
-              <div className="cart__info-wrap">
-                <div className="cart__name">{product.name}</div>
-                <div className="cart__description">{product.description}</div>
-              </div>
-              <div className="cart__price">{product.price} SEK</div>
-              <button
-                onClick={() => handleRemove(product.id)}
-                className="cart__remove"
-              >
-                Remove
-              </button>
-            </div>
-          );
-        })}
+            );
+          })}
       </div>
     </div>
   );
