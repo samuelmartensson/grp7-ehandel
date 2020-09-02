22aef1304795eab43ccde0db900ca2ebc16af104
madeleineenberg <madeleine_enberg@hotmail.com>
2020-09-02 15:31:28 +0200

    detailviewstyling

--- src/components/ProductDetailPage.jsx
+++ src/components/ProductDetailPage.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react';
-import { useState } from 'react';
-import Reviews from './Reviews';
+import React, { useEffect } from "react";
+import { useState } from "react";
+import Reviews from "./Reviews";
 
 export default function ProductDetailPage(props) {
-  const [product, setProduct] = useState('');
+  const [product, setProduct] = useState("");
   const productId = props.match.params.id;
   useEffect(() => {
     fetch(
@@ -16,22 +16,30 @@ export default function ProductDetailPage(props) {
   }, [productId]);
 
   function renderProduct() {
-    if (product === '') {
+    if (product === "") {
       return <div>loading</div>;
     } else {
       return (
-        <div>
-          <ol>
-            <li>{product.name}</li>
-            <li>{product.description}</li>
-            <li>{product.price}</li>
-            <li>{product.rating}</li>
-            <li>{product.stock}</li>
-            <li>
-              <img src={product.images[0].src.small} alt="" />
-            </li>
-          </ol>
-          <Reviews id={productId} />
+        <div className="detail">
+          <div className="detail__img-wrapper">
+            <img
+              className="detail__img"
+              src={product.images[0].src.small}
+              alt={product.images[0].alt}
+            />
+          </div>
+          <div className="detail__info">
+            <h1 className="detail__header">{product.name}</h1>
+            <ol className="detail__list">
+              <li className="detail__list-item">{product.description}</li>
+              <li className="detail__list-item">{`${product.price} kr `}</li>
+              <li className="detail__list-item">{`Rating: ${product.rating}`}</li>
+              <li className="detail__list-item">{`Stock: ${product.stock} `}</li>
+            </ol>
+          </div>
+          <div className="detail__reviews">
+            <Reviews id={productId} />
+          </div>
         </div>
       );
     }
